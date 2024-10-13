43002821d8b741f6677fbf5d6d4e37dbcff2314c
Niyaz Khasanov <khasanchik85@gmail.com>
2024-10-14 03:42:18 +0500

    Добавлен static

---
+++ dogs/templatetags/my_tags.py
@@ -0,0 +1,16 @@
+"""
+{% load %}
+Приложение должно содержать каталог templatetags, на том же уровне, что и models.py и т.д.
+Если такой каталог еще не существует, следует создать его, и не забыть про файл __init__.py,
+чтобы каталог рассматривался как пакет Пайтон"""
+
+
+from django import  template
+
+register = template.Library()
+
+@register.filter()
+def mymedia(val):
+    if val:
+        return  fr'/media/{val}'
+    return '/static/'
