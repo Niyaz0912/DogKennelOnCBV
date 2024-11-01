c629a04f132ae0a6fcb5b093ff2d839721320aea
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-01 15:37:22 +0500

    Добавлен функционал регистрации пользователя

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-                    path('admin/', admin.site.urls),
-                    path('', include('dogs.urls', namespace='dogs'))
+                  path('admin/', admin.site.urls),
+                  path('', include('dogs.urls', namespace='dogs')),
+                  path('users/', include('users.urls', namespace='users')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from django.shortcuts import render, reverse
+
+from users.models import User
+from users.forms import UserRegisterForm
+
+
+def user_register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponseRedirect(reverse('dog_index'))
+    return render(request, 'user/register_user.html', {'form': UserRegisterForm}, )
