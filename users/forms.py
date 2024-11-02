22cbefcc28a238a64bfa1697988b5600e5b3442d
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-02 23:39:36 +0500

    Добавлены: - шаблон - url - форма - и кнопка (из кабинета пользователя) для изменения пароля пользователя

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from users.models import User
 from users.validators import validate_password
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 class StyleFormMixin:
@@ -45,3 +46,7 @@ class UserUpdateForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'telegram', 'avatar',)
+
+
+class UserPasswordChangeForm(StyleFormMixin, PasswordChangeForm):
+    pass
--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 
-from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserForm
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm
 
 
 def user_register_view(request):
@@ -80,8 +80,8 @@ def user_update_view(request):
 @login_required
 def user_change_password_view(request):
     user_object = request.user
+    form = UserPasswordChangeForm(user_object, request.POST)
     if request.method == "POST":
-        form = UserPasswordChangeForm(user_object, request.POST)
         if form.is_valid():
             user_object = form.save()
             update_session_auth_hash(request, user_object)
@@ -90,11 +90,10 @@ def user_change_password_view(request):
         else:
             messages.error(request, "Не удалось изменить пароль!")
 
-    form = UserPasswordChangeForm()
     context = {
         'form': form
     }
-    return render(request, 'user/user_change_password.html', context)
+    return render(request, 'user/change_password_user.html', context)
 
 
 def user_logout_view(request):
