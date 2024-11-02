f358c921659fd48ff5a022832c53d16d75c9cd14
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-02 21:48:36 +0500

    В файл user\admin.py добавлено поле в is_active, Добавлена возможность поиска и установлена сортировка по умолчанию

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,6 @@ from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'last_name', 'first_name', 'pk', 'is_active')
-    list_filter = ('last_name',)
+    list_filter = ('last_name', 'is_active')  # Добавлено поле is_active для фильтрации
+    search_fields = ('email', 'last_name', 'first_name')  # Добавлена возможность поиска
+    ordering = ('last_name', 'first_name')  # Установлена сортировка по умолчанию
