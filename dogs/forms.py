9ef78a04991a13c96df9878605c1a028e195c2bc
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-07 12:30:48 +0500

    Добавлена блокировка на создание собаки старше 100 лет

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import forms
 
 from dogs.models import Dog
@@ -7,4 +9,16 @@ from users.forms import StyleFormMixin
 class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
-        fields = '__all__'
+        exclude = ('owner',)
+
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_date']
+        now_year = datetime.datetime.now().year
+        if now_year - cleaned_data.year > 100:
+            raise forms.ValidationError('Собака должна быть моложе 100 лет')
+        return cleaned_data
+
+# class DogCreateForm(StyleFormMixin, forms.ModelForm):
+#     class Meta:
+#         model = Dog
+#         exclude = ('owner',)
