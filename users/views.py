884b8209a573d799c10a6012ed6d3e0852eb0ba8
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-11 17:57:13 +0500

    fbv user_update_view переписано на cbv UserUpdateView

--- users/views.py
+++ users/views.py
@@ -13,19 +13,38 @@ from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 
 from users.models import User
-from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm, UserPasswordChangeForm, UserForm
 from users.services import send_register_email, send_new_password
 
 
 class UserRegisterView(CreateView):
     model = User
     form_class = UserRegisterForm
-    success_url = reverse_lazy('user:login_user')
-    template_name = 'user/register.html'
+    success_url = reverse_lazy('users:login_user')
+    template_name = 'user/register_user.html'
 
 
 class UserLoginView(LoginView):
     template_name = 'user/login_user.html'
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserForm
+    template_name = 'user/user_profile_read_only.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    template_name = 'user/update_user.html'
+    success_url = reverse_lazy('users:profile_user')
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 @login_required
