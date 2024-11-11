79e39fe3b17c6c810d9c1d1b194c4f28b44ea0a2
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-11 20:37:32 +0500

    Переделана часть проекта под dogs CBV

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from dogs.views import index, categories, category_dogs, dogs_list_view, dog_create_view, dog_detail_view, \
-    dog_update_view, dog_delete_view
+from dogs.views import index, categories, category_dogs, dog_create_view, dog_detail_view, \
+    dog_update_view, dog_delete_view, DogListView
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', index, name='index'),  # URL для отображения главной страницы питомника.
     path('categories/', categories, name='categories'),  # URL для отображения списка всех пород собак.
     path('categories/<int:pk>/dogs/', category_dogs, name='category_dogs'),  # URL для отображения пород по порядку pk.
-    path('dogs/', dogs_list_view, name='list_dogs'),  # URL для отображения всех собак питомника.
+    path('dogs/', DogListView.as_view(), name='list_dogs'),  # URL для отображения всех собак питомника.
     path('dogs/create/', dog_create_view, name='create_dog'),  # URL для создания новой собаки.
     path('dogs/detail/<int:pk>/', dog_detail_view, name='detail_dog'),  # URL для отображения детальной информации о конкретной собаке.
     path('dogs/update/<int:pk>/', dog_update_view, name='update_dog'),  # URL для обновления информации о конкретной собаке.
