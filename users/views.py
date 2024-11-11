cd5fd78942846e62769f8fce59cab972cfdcef68
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-11 20:27:36 +0500

    Были удалены импорты неиспользуемых модулей

--- users/views.py
+++ users/views.py
@@ -1,15 +1,10 @@
 import random
 import string
 
-from django.http import HttpResponseRedirect, HttpResponse
-
 from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
 from django.views.generic import CreateView, UpdateView
-from django.contrib import messages
-from django.shortcuts import render, reverse, redirect
-from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.shortcuts import reverse, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 
 from users.models import User
@@ -53,36 +48,8 @@ class UserPasswordChangeView(PasswordChangeView):
     success_url = reverse_lazy('users:profile_user')
 
 
-@login_required
-def user_profile_view(request):
-    user_object = request.user
-    context = {
-        # 'user_object': user_object,
-        'title': f'Ваш профиль {user_object.first_name}',
-        # 'form': UserForm(instance=user_object),
-    }
-    return render(request, 'user/user_profile_read_only.html', context)
-
-
-@login_required
-def user_update_view(request):
-    user_object = request.user
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, request.FILES, instance=user_object)
-        if form.is_valid():
-            user_object = form.save()
-            user_object.save()
-            return HttpResponseRedirect(reverse('users:profile_user'))
-    user_name = user_object.first_name
-    context = {
-        'user_object': user_object,
-        'title': f'Изменить профиль {user_name}',
-        'form': UserUpdateForm(instance=user_object)
-    }
-    return render(request, 'user/update_user.html', context)
-
-
-
+class UserLogoutView(LogoutView):
+    template_name = 'user/logout_user.html'
 
 
 @login_required
@@ -92,7 +59,3 @@ def user_generate_new_password(request):
     request.user.save()
     send_new_password(request.user.email, new_password)
     return redirect(reverse('dogs:index'))
-
-
-class UserLogoutView(LogoutView):
-    template_name = 'user/logout_user.html'
