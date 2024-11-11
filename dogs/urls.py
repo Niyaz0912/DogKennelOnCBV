beb9e073af7873f60472b0a40273d6cd92844000
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-11 21:43:47 +0500

    dog_detail_view >> DogDetailViev, внесены изменения в urls.py, views.py

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from dogs.views import index, categories, category_dogs, dog_detail_view, \
-    dog_update_view, dog_delete_view, DogListView, DogCreateView
+from dogs.views import index, categories, category_dogs, dog_update_view, dog_delete_view, DogListView, DogCreateView, DogDetailView
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
-# URL-конфигурация для приложения 'dogs'.
-# Эти URL-шаблоны определяют маршруты для различных представлений приложения.
 
 urlpatterns = [
     path('', index, name='index'),  # URL для отображения главной страницы питомника.
@@ -13,7 +10,7 @@ urlpatterns = [
     path('categories/<int:pk>/dogs/', category_dogs, name='category_dogs'),  # URL для отображения пород по порядку pk.
     path('dogs/', DogListView.as_view(), name='list_dogs'),  # URL для отображения всех собак питомника.
     path('dogs/create/', DogCreateView.as_view(), name='create_dog'),  # URL для создания новой собаки.
-    path('dogs/detail/<int:pk>/', dog_detail_view, name='detail_dog'),  # URL для отображения детальной информации о конкретной собаке.
+    path('dogs/detail/<int:pk>/', DogDetailView.as_view(), name='detail_dog'),  # URL для отображения детальной информации о конкретной собаке.
     path('dogs/update/<int:pk>/', dog_update_view, name='update_dog'),  # URL для обновления информации о конкретной собаке.
     path('dogs/delete/<int:pk>/', dog_delete_view, name='delete_dog'),  # URL для удаления собаки.
 ]
