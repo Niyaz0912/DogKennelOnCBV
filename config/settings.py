710fd042606021310fd2e8e06b62bca69ca3efe4
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-12 16:51:51 +0500

    Осуществлена логика для кеширования некоторых страниц приложения.

--- config/settings.py
+++ config/settings.py
@@ -153,6 +153,16 @@ LOGIN_REDIRECT_URL = 'dogs:index'  # '/'
 # LOGOUT_REDIRECT_URL = 'dogs:index'  # '/'
 LOGIN_URL = '/user/'
 
+# CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+# if CACHE_ENABLED:
+#     CACHES = {
+#         'default': {
+#             "BACKEND": "django.core.cache.backends.redis.RedisCache",
+#             "LOCATION": os.getenv("CACHE_LOCATION")
+#         }
+#     }
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465
--- dogs/urls.py
+++ dogs/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page, never_cache
 from dogs.views import index, categories, category_dogs, DogListView, DogCreateView, \
     DogDetailView, DogUpdateView, DogDeleteView
 from dogs.apps import DogsConfig
@@ -6,12 +7,12 @@ from dogs.apps import DogsConfig
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),  # URL для отображения главной страницы питомника.
-    path('categories/', categories, name='categories'),  # URL для отображения списка всех пород собак.
+    path('', cache_page(60)(index), name='index'),  # URL для отображения главной страницы питомника.
+    path('categories/', cache_page(60)(categories), name='categories'),  # URL для отображения списка всех пород собак.
     path('categories/<int:pk>/dogs/', category_dogs, name='category_dogs'),  # URL для отображения пород по порядку pk.
     path('dogs/', DogListView.as_view(), name='list_dogs'),  # URL для отображения всех собак питомника.
     path('dogs/create/', DogCreateView.as_view(), name='create_dog'),  # URL для создания новой собаки.
     path('dogs/detail/<int:pk>/', DogDetailView.as_view(), name='detail_dog'),  # URL для отображения детальной информации о конкретной собаке.
-    path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='update_dog'),  # URL для обновления информации о конкретной собаке.
+    path('dogs/update/<int:pk>/', never_cache(DogUpdateView.as_view()), name='update_dog'),  # URL для обновления информации о конкретной собаке.
     path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='delete_dog'),  # URL для удаления собаки.
 ]
