bac5d7cff0add676f26aab6d776311d7abc475d7
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-14 20:04:34 +0500

    DogDeactivateListView (показывает только отключенные объекты, в зависимости от уровня доступа)

--- dogs/views.py
+++ dogs/views.py
@@ -8,6 +8,7 @@ from django.forms import inlineformset_factory
 
 from dogs.models import Category, Dog, Parent
 from dogs.forms import DogForm, ParentForm
+from users.models import UserRoles
 
 
 def index(request):
@@ -43,6 +44,27 @@ class DogListView(ListView):
         'title': 'Питомник - Все наши собаки',
     }
     template_name = 'dogs/dogs.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_active=True)
+        return queryset
+
+
+class DogDeactivateListView(LoginRequiredMixin, ListView):
+    model = Dog
+    extra_context = {
+        'title': 'Питомник - неактивные собаки',
+    }
+    template_name = 'dogs/dogs.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.role in [UserRoles.MODERATOR, UserRoles.ADMIN]:
+            queryset = queryset.filter(is_active=False)
+        if self.request.user.role == UserRoles.USER:
+            queryset = queryset.filter(is_active=False, owner=self.request.user)
+        return queryset
 
 
 class DogCreateView(LoginRequiredMixin, CreateView):
