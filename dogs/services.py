20a4198261f757b593b4cb3bc6a17fddd9989cb1
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-16 17:48:19 +0500

    Добавлен сервис отправки сообщений владельцу питомца о просмотрах объявления

--- dogs/services.py
+++ dogs/services.py
@@ -1,17 +1,28 @@
-# from django.conf import settings
-# from django.core.cache import cache
-#
-# from dogs.models import Category
-#
-#
-# def get_categories_cache():
-#     if settings.CACHE_ENABLED:
-#         key = 'category_list'
-#         category_list = cache.get(key)
-#         if category_list is None:
-#             category_list = Category.objects.all()
-#             cache.set(key, category_list)
-#     else:
-#         category_list = Category.objects.all()
-#
-#     return category_list
+from django.conf import settings
+from django.core.cache import cache
+from django.core.mail import send_mail
+
+from dogs.models import Category
+from dogs.models import Dog
+
+
+def get_categories_cache():
+    if settings.CACHE_ENABLED:
+        key = 'category_list'
+        category_list = cache.get(key)
+        if category_list is None:
+            category_list = Category.objects.all()
+            cache.set(key, category_list)
+    else:
+        category_list = Category.objects.all()
+
+    return category_list
+
+
+def send_views_mail(dog_object, owner_email, views_count):
+    send_mail(
+        subject=f'{views_count} просмотров {dog_object}',
+        message=f'Юхуу! Уже {views_count}, просмотров записи {dog_object}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[owner_email, ]
+    )
