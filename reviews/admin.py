988b9136f0def38a41fa97943b6e63b59ec0103b
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-17 20:14:17 +0500

    Добавлена модель отзыва, зарегистрирована в админке, написана форма для нее

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'dog', 'autor', 'created', 'sign_of_review',)
+    ordering = ('created',)
+    list_filter = ('dog', 'autor',)
