29920eef159fd42b95fdb3fc8fd104b711d211fc
Niyaz Khasanov <khasanchik85@gmail.com>
2024-11-17 20:15:14 +0500

    Написаны представления для отзывов

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from reviews.models import Review
 
-# Create your views here.
+
+class DogReviewListView(LoginRequiredMixin, ListView):
+    model = Review
+    extra_context = {
+        'title': 'Все отзывы о собаке'
+    }
+    template_name = 'reviews/reviews.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
+        queryset = queryset.filter(sign_of_review=True)
+
+        return queryset
+
+
+class DeactivatedDogReviewListView(LoginRequiredMixin, ListView):
+    model = Review
+    extra_context = {
+        'title': 'Невктивные отзывы'
+    }
+    template_name = 'reviews/reviews.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(dog_pk=self.kwargs.get('pk'))
+        queryset = queryset.filter(sign_of_review=False)
+
+        return queryset
